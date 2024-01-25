b9411a91cc7ed5c34be1212bb37b8724ab2b9e67
Samuel Stanton <ss13641@nyu.edu>
2024-01-25 07:05:11 -0500

    rename sine pos encoder

--- cortex/model/elemental/__init__.py
+++ cortex/model/elemental/__init__.py
@@ -2,4 +2,4 @@ from ._apply import Apply
 from ._expression import Expression
 from ._functional import identity, permute_spatial_channel_dims
 from ._layernorm import MaskLayerNorm1d
-from _sine_pos_encoding import SinePosEncoder
+from cortex.model.elemental._sine_pos_encoder import SinePosEncoder
