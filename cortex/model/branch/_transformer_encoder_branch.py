b74ccc393dd7ec6b1c55db3f0f0930232109805f
Samuel Stanton <sdstanton1@gmail.com>
2025-05-09 20:58:21 -0400

    add simple transformer encoder branch

---
+++ cortex/model/branch/_transformer_encoder_branch.py
@@ -0,0 +1,97 @@
+from dataclasses import dataclass
+
+import torch
+from torch import nn
+
+from cortex.model.block import TransformerEncoderBlock
+from cortex.model.branch import BranchNode, BranchNodeOutput
+from cortex.model.elemental import (
+    Apply,
+    Expression,
+    MeanPooling,
+    WeightedMeanPooling,
+    identity,
+)
+from cortex.model.trunk import PaddedTrunkOutput
+
+
+@dataclass
+class TransformerEncoderBranchOutput(BranchNodeOutput):
+    branch_mask: torch.Tensor
+    pooled_features: torch.Tensor
+
+
+class TransformerEncoderBranch(BranchNode):
+    """
+    Branch node which transforms aggregated trunk features to task branch specific features
+    """
+
+    def __init__(
+        self,
+        in_dim: int,
+        out_dim: int = 64,
+        channel_dim: int = 64,
+        num_blocks: int = 2,
+        num_heads: int = 5,
+        dropout_prob: float = 0.0,
+        pooling_type: str = "mean",
+        **kwargs,
+    ):
+        super().__init__()
+        # create encoder
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.channel_dim = channel_dim
+        self.num_blocks = num_blocks
+
+        if num_blocks == 0:
+            # add projection if dims don't match
+            encoder_modules = [
+                Expression(identity) if in_dim == out_dim else Apply(nn.Linear(in_dim, out_dim, bias=False))
+            ]
+        else:
+            # conv layers expect inputs with shape (batch_size, input_dim, num_tokens)
+            encoder_modules = []
+
+        if num_blocks == 1:
+            encoder_modules.append(TransformerEncoderBlock(in_dim, out_dim, num_heads, dropout_p=dropout_prob))
+        elif num_blocks > 1:
+            encoder_modules.append(TransformerEncoderBlock(in_dim, channel_dim, num_heads, dropout_p=dropout_prob))
+            encoder_modules.extend(
+                [
+                    TransformerEncoderBlock(channel_dim, channel_dim, num_heads, dropout_p=dropout_prob)
+                    for _ in range(num_blocks - 2)
+                ]
+            )
+            encoder_modules.append(TransformerEncoderBlock(channel_dim, out_dim, num_heads, dropout_p=dropout_prob))
+
+        self.encoder = nn.Sequential(*encoder_modules)
+        if pooling_type == "mean":
+            self.pooling_op = MeanPooling()
+        elif pooling_type == "weighted_mean":
+            self.pooling_op = WeightedMeanPooling(out_dim)
+        else:
+            raise NotImplementedError
+
+    def forward(
+        self,
+        trunk_outputs: PaddedTrunkOutput,
+    ) -> TransformerEncoderBranchOutput:
+        """
+        Args:
+            trunk_outputs: {'trunk_features': torch.Tensor, 'padding_mask': torch.Tensor}
+        Returns:
+            outputs: {'branch_features': torch.Tensor, 'branch_mask': torch.Tensor, 'pooled_features': torch.Tensor}
+        """
+        trunk_features = trunk_outputs.trunk_features
+        padding_mask = trunk_outputs.padding_mask
+
+        branch_features, branch_mask = self.encoder((trunk_features, padding_mask.to(trunk_features)))
+        pooled_features = self.pooling_op(branch_features, branch_mask)
+
+        branch_outputs = TransformerEncoderBranchOutput(
+            branch_features=branch_features.contiguous(),
+            branch_mask=branch_mask,
+            pooled_features=pooled_features,
+        )
+        return branch_outputs
