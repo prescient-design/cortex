cb3bcac1bbcf0b3a670a1bfb106a8b2b5756e6d1
Samuel Stanton <ss13641@nyu.edu>
2025-05-12 10:41:34 -0400

    fix bad isort

--- cortex/data/dataset/__init__.py
+++ cortex/data/dataset/__init__.py
@@ -1,9 +1,11 @@
 from ._data_frame_dataset import DataFrameDataset, ordered_dict_collator
 from ._numpy_dataset import NumpyDataset
 from ._rfp_dataset import RedFluorescentProteinDataset
-from ._tape_combined import TAPECombinedDataset
 from ._tape_fluorescence import TAPEFluorescenceDataset
 from ._tape_stability import TAPEStabilityDataset
+
+# ruff: noqa: I001
+from ._tape_combined import TAPECombinedDataset
 from ._transformed_dataset import TransformedDataset
 
 __all__ = [
